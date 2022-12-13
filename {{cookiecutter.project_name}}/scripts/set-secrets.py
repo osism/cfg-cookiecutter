39f6803cf52e317beadf8e53562795577b4c68b5
Christian Berendt <berendt@osism.tech>
2022-12-13 14:18:04 +0100

    Add netbox secrets/tokens
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- {{cookiecutter.project_name}}/scripts/set-secrets.py
+++ {{cookiecutter.project_name}}/scripts/set-secrets.py
@@ -6,13 +6,14 @@ import string
 from passlib.hash import sha512_crypt
 from ruamel.yaml import YAML
 
-SECRETS_ALL = {
-    'keystone_admin_password': 'keystone_admin_password'
-}
+SECRETS_ALL = {"keystone_admin_password": "keystone_admin_password"}
 
-SECRETSFILE_INPUT = 'environments/kolla/secrets.yml'
-SECRETSFILE_OUTPUT_ALL = 'environments/secrets.yml'
-SECRETSFILE_OUTPUT_KOLLA = 'environments/kolla/secrets.yml'
+SECRETSFILE_INPUT = "environments/kolla/secrets.yml"
+
+SECRETSFILE_OUTPUT_ALL = "environments/secrets.yml"
+SECRETSFILE_OUTPUT_INFRASTRUCTURE = "environments/kolla/secrets.yml"
+SECRETSFILE_OUTPUT_KOLLA = "environments/kolla/secrets.yml"
+SECRETSFILE_OUTPUT_MANAGER = "environments/kolla/secrets.yml"
 
 yaml = YAML()
 yaml.explicit_start = True
@@ -26,38 +27,68 @@ with open(SECRETSFILE_INPUT) as fp:
 with open(SECRETSFILE_OUTPUT_ALL) as fp:
     secrets_output_all = yaml.load(fp)
 
+with open(SECRETSFILE_OUTPUT_INFRASTRUCTURE) as fp:
+    secrets_output_infrastructure = yaml.load(fp)
+
 with open(SECRETSFILE_OUTPUT_KOLLA) as fp:
     secrets_output_kolla = yaml.load(fp)
+
+with open(SECRETSFILE_OUTPUT_MANAGER) as fp:
+    secrets_output_manager = yaml.load(fp)
 
 for key in SECRETS_ALL.keys():
     secrets_output_all[key] = secrets_input[SECRETS_ALL[key]]
 
-if 'ceph_cluster_fsid' in secrets_output_kolla:
-    del secrets_output_kolla['ceph_cluster_fsid']
+if "ceph_cluster_fsid" in secrets_output_kolla:
+    del secrets_output_kolla["ceph_cluster_fsid"]
 
-secrets_output_all['ara_server_mariadb_password'] = ''.join([
-    random.SystemRandom().choice(
-        string.ascii_letters + string.digits)
-    for n in range(16)
-])
-secrets_output_all['ara_password'] = ''.join([
-    random.SystemRandom().choice(
-        string.ascii_letters + string.digits)
-    for n in range(16)
-])
+secrets_output_all["ara_server_mariadb_password"] = "".join(
+    [
+        random.SystemRandom().choice(string.ascii_letters + string.digits)
+        for n in range(16)
+    ]
+)
+secrets_output_all["ara_password"] = "".join(
+    [
+        random.SystemRandom().choice(string.ascii_letters + string.digits)
+        for n in range(16)
+    ]
+)
 
-operator_password = ''.join([
-    random.SystemRandom().choice(
-        string.ascii_letters + string.digits)
-    for n in range(16)
-])
-secrets_output_all['operator_password_unhashed'] = operator_password
-secrets_output_all['operator_password'] = sha512_crypt.hash(operator_password)
+operator_password = "".join(
+    [
+        random.SystemRandom().choice(string.ascii_letters + string.digits)
+        for n in range(16)
+    ]
+)
+secrets_output_all["operator_password_unhashed"] = operator_password
+secrets_output_all["operator_password"] = sha512_crypt.hash(operator_password)
 secrets_output_all.yaml_add_eol_comment(operator_password,
-                                        key='operator_password')
+                                        key="operator_password")
 
-with open(SECRETSFILE_OUTPUT_ALL, 'w+') as fp:
+netbox_api_token = "".join(
+    [random.SystemRandom().choice(string.digits) for n in range(40)]
+)
+secrets_output_manager["netbox_api_token"] = netbox_api_token
+secrets_output_infrastructure["netbox_user_api_token"] = netbox_api_token
+secrets_output_infrastructure["netbox_secret_key"] = "".join(
+    [
+        random.SystemRandom().choice(string.ascii_letters + string.digits)
+        for n in range(16)
+    ]
+)
+secrets_output_infrastructure["netbox_superuser_api_token"] = "".join(
+    [random.SystemRandom().choice(string.digits) for n in range(40)]
+)
+
+with open(SECRETSFILE_OUTPUT_ALL, "w+") as fp:
     yaml.dump(secrets_output_all, fp)
 
-with open(SECRETSFILE_OUTPUT_KOLLA, 'w+') as fp:
+with open(SECRETSFILE_OUTPUT_INFRASTRUCTURE, "w+") as fp:
+    yaml.dump(secrets_output_infrastructure, fp)
+
+with open(SECRETSFILE_OUTPUT_KOLLA, "w+") as fp:
     yaml.dump(secrets_output_kolla, fp)
+
+with open(SECRETSFILE_OUTPUT_MANAGER, "w+") as fp:
+    yaml.dump(secrets_output_manager, fp)
